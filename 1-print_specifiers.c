fb46d3aa2495e667c046e5c0d947697bb763254d
shaffy95 <onozareshaffy95@gmail.com>
2023-08-20 03:18:05 -0700

    update

---
+++ 1-print_specifiers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * handle_specifiers - Handle the following conversion specifiers:
+ *                     d - i
+ *
+ * Description: This function is responsible for handling the 'd' and 'i'
+ *              conversion specifiers for integer values. It doesn't consider
+ *              flag characters, field width, precision, or length modifiers.
+ *              It is part of a larger printf-like function and focuses only
+ *              on formatting integers using the specified conversion specifiers.
+ *
+ * Return: The number of characters printed.
+ */
+int handle_specifiers(void)
+{
+	_printf("Handle the following conversion specifiers:\n" "d\n" "i\n");
+
+	return (0);
+}
+

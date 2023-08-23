71334b414cfd01a2c3a4e44ef72054839c259a10
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-23 06:29:32 +0100

    _printf

--- print_specifiers3.c
+++ print_specifiers3.c
@@ -10,17 +10,23 @@ int print_hex_binary(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
 	int i, count = 0;
+	int significant_digits = sizeof(num) * 8;
 
-	int printed = 0;
-
-	for (i = sizeof(num) * 8 - 1; i >= 0; i--)
+	for (i = significant_digits - 1; i >= 0; i--)
 	{
-		if ((num & (1u << i)) || printed)
-		{
-			putchar((num & (1u << i)) ? '1' : '0');
-			count++;
-			printed = 1;
-		}
+		if (num & (1u << i))
+			break;
+	}
+	if (i < 0)
+	{
+		putchar('0');
+		return (1);
+	}
+
+	for (; i >= 0; i--)
+	{
+		putchar((num & (1u << i)) ? '1' : '0');
+		count++;
 	}
 	return (count);
 }
